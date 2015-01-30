8e700756215af419206e912955044a0b329562f8
KytioisaCat <joel.ronnqvist@gmail.com>
2015-01-30 16:31:39 +0100

    Datalager model complete and fill.sql half-done but abandoned

--- datalager.sql
+++ datalager.sql
@@ -1,26 +1,46 @@
+DROP TABLE IF EXISTS transaktion;
 DROP TABLE IF EXISTS station;
 DROP TABLE IF EXISTS passage;
-DROP TABLE IF EXISTS bil;
+DROP TABLE IF EXISTS fordon;
+DROP TABLE IF EXISTS tid;
+
+CREATE TABLE transaktion(
+	station_id SMALLINT,
+	fordon_id TINYINT,
+	passage_id SMALLINT,
+	tidpunkt_id INT,
+	co2	SMALLINT NOT NULL,
+	pris SMALLINT NOT NULL,
+	FOREIGN KEY(station_id) REFERENCES station(station_id),
+	FOREIGN KEY(fordon_id) REFERENCES fordon(fordon_id),
+	FOREIGN KEY(passage_id) REFERENCES passage(passage_id),
+	FOREIGN KEY(tidpunkt_id) REFERENCES tidpunkt(tidpunkt_id)
+);
 
 CREATE TABLE station(
-	station_id 	INT		PRIMARY KEY NOT NULL,
+	station_id 	SMALLINT	PRIMARY KEY,
 	gatunamn 	CHAR(50) NOT NULL,
 	stadsdel	CHAR(50) NOT NULL,
 	stad	CHAR(50) NOT NULL
 );
 
-CREATE TABLE passage(
-	passage_id 	LONG		PRIMARY KEY NOT NULL,
-	tidpunkt	DATETIME NOT NULL,
-	pris	REAL NOT NULL,
-	station_id INT NOT NULL,
-	FOREIGN KEY(station_id) REFERENCES station(station_id)
+CREATE TABLE fordon(
+	passage_id SMALLINT,
+	fordon_id TINYINT PRIMARY KEY,
+	klass 	CHAR(1) NOT NULL
 );
 
-CREATE TABLE bil(
-	klass 	CHAR(10) NOT NULL,
-	drivmedel 	CHAR(1) NOT NULL,
-	co2	INT NOT NULL,
-	passage_id LONG NOT NULL,
-	FOREIGN KEY(passage_id) REFERENCES passage(passage_id)
+CREATE TABLE passage(
+	passage_id INT PRIMARY KEY,
+	station_id SMALLINT,
+	stadsdel CHAR(25),
+	FOREIGN KEY (station_id) REFERENCES station(station_id)
+);
+
+CREATE TABLE tid(
+	tidpunkt_id INT PRIMARY KEY,
+	år SMALLINT NOT NULL,
+	månad TINYINT NOT NULL,
+	dag TINYINT NOT NULL,
+	timme TINYINT NOT NULL
 );
---
+++ fill.sql
@@ -0,0 +1,11 @@
+INSERT INTO station( gatunamn, stadsdel, stad) VALUES ("Svartgatan 19", "Västra Hisingen", "Göteborg");
+INSERT INTO station( gatunamn, stadsdel, stad) VALUES ("Gröngatan 10", "Angered", "Göteborg");
+INSERT INTO station( gatunamn, stadsdel, stad) VALUES ("Rosagatan 88", "Askim-Frölunda-Högsbo", "Göteborg");
+INSERT INTO station( gatunamn, stadsdel, stad) VALUES ("Gulgatan 40", "Majorna-Linné", "Göteborg");
+INSERT INTO station( gatunamn, stadsdel, stad) VALUES ("Blågatan 90", "Östra Göteborg", "Göteborg");
+
+INSERT INTO fordon(klass) VALUES ("Tung Lastbil");
+INSERT INTO fordon(klass) VALUES ("Lätt Lastbil");
+INSERT INTO fordon(klass) VALUES ("Taxi");
+INSERT INTO fordon(klass) VALUES ("Buss");
+INSERT INTO fordon(klass) VALUES ("Personbil");
